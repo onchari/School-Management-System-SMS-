ca135645bbccb2af4e7d304b8f6dffecbef25e78
onchari wycliffe <onchariwycliffe@gmail.com>
2017-07-05 07:28:47 +0300

    1st changes

--- School Management System/Controllers/HomeController.cs
+++ School Management System/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace School_Management_System.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Yn page.";
 
             return View();
         }
